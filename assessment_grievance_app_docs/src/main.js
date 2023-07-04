e018a7124983294e390c8ba217d575d3eed6b815
derek-baker <dbaker@sdgnys.com>
2023-07-04 13:39:03 -0400

    Remove AG app docs and add NextJS tutorial code

--- assessment_grievance_app_docs/src/main.js
+++
@@ -1,7 +0,0 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import 'bootstrap/dist/js/bootstrap.bundle';
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App)
-    .mount('#app')
