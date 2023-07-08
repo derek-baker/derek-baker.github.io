faf551c3f52f5be9581c7a9b66b2b07c5916d490
derek-baker <dbaker@sdgnys.com>
2023-07-08 15:05:41 -0400

    Update Next config for static build output

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports#configuration
+  output: 'export',
+  distDir: '../dist'
 }
 
 module.exports = nextConfig
